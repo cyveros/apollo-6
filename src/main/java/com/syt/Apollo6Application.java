4dfc51bf1e3dafb2e3c41c90439d489cb363b2d3
cyveros <cyveros@gmail.com>
2017-01-24 23:23:08 -0500

    fixes - add all files

---
+++ src/main/java/com/syt/Apollo6Application.java
@@ -0,0 +1,12 @@
+package com.syt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apollo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apollo6Application.class, args);
+	}
+}
---
+++ src/main/java/com/syt/web/AuthController.java
@@ -0,0 +1,14 @@
+package com.syt.web;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+}
